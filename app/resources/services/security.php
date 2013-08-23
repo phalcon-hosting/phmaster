fefa2b27f89464fc5b4d277e06b81431f9e9dd68
thecodeassassin <hoogendijk09@gmail.com>
2013-08-23 14:17:49 +0200

    Update security.php

--- app/resources/services/security.php
+++ app/resources/services/security.php
@@ -6,21 +6,19 @@
 $di->set('dispatcher', function() use ($di) {
 
 
-//   //Obtain the standard eventsManager from the DI
+    //Obtain the standard eventsManager from the DI
     $eventsManager = $di->getShared('eventsManager');
 
-//     //Instantiate the Security plugin
+    // Instantiate the Security plugin
      $security = new \PH\Master\Security($di);
 
-//     //Listen for events produced in the dispatcher using the Security plugin
+    //Listen for events produced in the dispatcher using the Security plugin
      $eventsManager->attach('dispatch', $security);
 
      $dispatcher = new Phalcon\Mvc\Dispatcher();
 
-//     //Bind the EventsManager to the Dispatcher
+   //Bind the EventsManager to the Dispatcher
      $dispatcher->setEventsManager($eventsManager);
-//
-
 
      return $dispatcher;
  });
